3528e696876746984d47ce842c4c9d38cb84c515
Markus Antoni Suwignyo <markus.suwignyo@traveloka.com>
2023-02-25 19:06:01 +0700

    initial commit

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,21 @@
 import * as React from "react";
-export const Button = () => {
-  return <button>Boop</button>;
+
+interface Props {
+  primary?: boolean;
+  size?: "small" | "large";
+  label?: string;
+}
+
+export const Button = (props: Props) => {
+  const { label, primary, size } = props;
+  return (
+    <button
+      style={{
+        backgroundColor: primary ? "red" : "blue",
+        fontSize: size === "large" ? "24px" : "14px",
+      }}
+    >
+      {label}
+    </button>
+  );
 };
